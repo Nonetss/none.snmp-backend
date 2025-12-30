4f260d6bd4eccbfe4da61a99730195972f61c71e
nonete <antonio.moreno@sjd.es>
2025-12-30 20:31:58 +0000

    feat: Add route tracing API and migrate `vlan` table port columns to text type.

---
+++ drizzle/20251230202349_hesitant_tinkerer/migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "vlan" ALTER COLUMN "egress_ports" SET DATA TYPE text USING "egress_ports"::text;--> statement-breakpoint
+ALTER TABLE "vlan" ALTER COLUMN "untagged_ports" SET DATA TYPE text USING "untagged_ports"::text;
