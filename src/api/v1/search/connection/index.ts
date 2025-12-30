9a95f2e4bc136dfefe245d5f20fb4be7035fa192
nonete <antonio.moreno@sjd.es>
2025-12-30 19:56:43 +0000

    Delete switch graph functionality

--- src/api/v1/search/connection/index.ts
+++ src/api/v1/search/connection/index.ts
@@ -1,10 +1,8 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
-import switchGraphRouter from './graph/get.index';
 import connectionSearchRouter from './get/get.index';
 
 const connectionRouter = new OpenAPIHono();
 
-connectionRouter.route('/', switchGraphRouter);
 connectionRouter.route('/', connectionSearchRouter);
 
 export default connectionRouter;
