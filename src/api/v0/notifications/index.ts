dea7cfa22961e1fa8a038ba0e6450adce5213708
nonete <antonio.moreno@sjd.es>
2026-01-08 20:13:04 +0000

    Add actionRouter to notificationsRouter routes

--- src/api/v0/notifications/index.ts
+++ src/api/v0/notifications/index.ts
@@ -1,10 +1,12 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import credentialRouter from './credential';
 import topicRouter from './topic';
+import actionRouter from './action';
 
 const notificationsRouter = new OpenAPIHono();
 
 notificationsRouter.route('/credential', credentialRouter);
 notificationsRouter.route('/topic', topicRouter);
+notificationsRouter.route('/action', actionRouter);
 
 export default notificationsRouter;
