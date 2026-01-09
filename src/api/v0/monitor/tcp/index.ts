1e07042ea7fd591070a668d63565e89ad422f172
Nonete <amorenolopezbarajas@pm.me>
2026-01-09 17:00:43 +0100

    Remove subnet-scan functionality from TCP monitoring

--- src/api/v0/monitor/tcp/index.ts
+++ src/api/v0/monitor/tcp/index.ts
@@ -1,12 +1,10 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import getRouter from './get/get.index';
 import scanRouter from './scan/scan.index';
-import subnetScanRouter from './subnet-scan/scan.index';
 
 const tcpRouter = new OpenAPIHono();
 
 tcpRouter.route('/', getRouter);
 tcpRouter.route('/', scanRouter);
-tcpRouter.route('/', subnetScanRouter);
 
 export default tcpRouter;
--- src/api/v0/monitor/tcp/subnet-scan/scan.index.ts
+++
@@ -1,7 +0,0 @@
-import { OpenAPIHono } from '@hono/zod-openapi';
-import { postSubnetPortScanRoute } from './scan.route';
-import { postSubnetPortScanHandler } from './scan.handler';
-
-const router = new OpenAPIHono();
-router.openapi(postSubnetPortScanRoute, postSubnetPortScanHandler);
-export default router;
