99b229dafad6ffe742bf01b1e1ac272ebf3823df
Nonete <amorenolopezbarajas@pm.me>
2026-01-13 18:16:08 +0100

    Add list functionality to Komodo container API

--- src/api/v0/komodo/index.ts
+++ src/api/v0/komodo/index.ts
@@ -1,7 +1,7 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import komodoServerRouter from './server';
 import komodoStackRouter from './stacks';
-import komodoContainerRouter from './container/get.index';
+import komodoContainerRouter from './container';
 import komodoAuthRouter from './auth';
 
 const komodoRouter = new OpenAPIHono();
